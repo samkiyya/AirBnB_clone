f44a94b4f6ba0e396bcf7d2d881dd25452ab0d21
Omar Jammeh <orms45@gmail.com>
2023-07-15 18:01:26 +0000

    Updated: tests/test_model/test_base_model
    Added: Test case for the string representation of the basemodel object

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -25,12 +25,13 @@ class TestBaseModel(unittest.TestCase):
 
     def test_doc(self):
         """ Tests doc """
-        self.assertisNotNone(BaseModel.__doc__)
+        self.assertIsNotNone(BaseModel.__doc__)
 
     def test_to_json(self):
         '''test to jason'''
 
     def test_kwarg(self):
+        """ Test kwargs """
         basemodel = BaseModel(name="base")
         self.assertEqual(type(basemodel).__name__, "BaseModel")
         self.assertTrue(hasattr(basemodel, "id"))
@@ -42,7 +43,7 @@ class TestBaseModel(unittest.TestCase):
     def test_string_rep(self):
         """ Test string representation of basemodel """
         basemodel = BaseModel()
-        self.assertEqual(basemodel.__str__, "[{}] ({}) {}".format("BaseModel", basemodel.id, basemodel.__dict__))
+        self.assertEqual(str(basemodel), "[BaseModel] ({}) {}".format(basemodel.id, basemodel.__dict__))
 
     def test_unique_id(self):
         """ Test unique ids """
